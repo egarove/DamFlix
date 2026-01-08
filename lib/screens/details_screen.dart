36b778264f1753d776b396428dbadec324fd2672
eloy garofano <eloygarov@gmail.com>
2026-01-08 10:01:04 +0100

    details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -6,12 +6,38 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Details Screen"),
-      ),
-      body: Column(
-        children: [],
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar() //queremos un appbar con tamaño personalizado
+        ],
       )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200, 
+      floating: false,
+      pinned: true,
+      snap: false,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          child: Container(child: Text("Spiderman: Coming Home"), color: Colors.white38, width: double.infinity,)
+        ),
+        centerTitle: true,
+        background: FadeInImage(
+          placeholder: AssetImage("assets/no-image-icon-6.png"),
+          image: NetworkImage('https://es.web.img2.acsta.net/pictures/17/06/19/14/01/130456.jpg'),
+          fit: BoxFit.cover,
+          ),
+      ),
     );
   }
 }
