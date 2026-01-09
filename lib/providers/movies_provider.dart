9c61d36996e8539e7fa65494767c2f25b67e5ca5
eloy garofano <eloygarov@gmail.com>
2026-01-09 10:14:22 +0100

    getonDisplayMovies() incompleto da error

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+
+  String _apiKey = 'a68d44ce7d59d9f3c283182f4af07a16';
+  String _baseUrl = 'api.thememoviedb.org';
+  String _language = 'es-Es';
+
+
   MoviesProvider(){
     print("Movies Provider ha sido inicializado");
     this.getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print("Getting on display movies");
+    //obtenemos los datos a traves de la API
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing?language=es-ES&page=1', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': 1
+    });
+    var response = await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+
+    print(response.body);
   }
 }
