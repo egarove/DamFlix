33c4622a31fd62f2fd1d42716956798864fe4976
eloy garofano <eloygarov@gmail.com>
2026-01-13 09:22:58 +0100

    arreglo el provider para que descargue correctamente el json con las peliculas

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -4,24 +4,26 @@ import 'package:http/http.dart' as http;
 class MoviesProvider extends ChangeNotifier{
 
   String _apiKey = 'a68d44ce7d59d9f3c283182f4af07a16';
-  String _baseUrl = 'api.thememoviedb.org';
-  String _language = 'es-Es';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
 
 
-  MoviesProvider(){
-    print("Movies Provider ha sido inicializado");
+  MoviesProvider() {
+    print('MoviesProvider ha sido inicializado');
+
     this.getOnDisplayMovies();
+
   }
 
   getOnDisplayMovies() async {
-    //obtenemos los datos a traves de la API
-    var url = Uri.https(_baseUrl, '/3/movie/now_playing?language=es-ES&page=1', {
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
       'api_key': _apiKey,
       'language': _language,
-      'page': 1
-    });
+      'page': '1'
+    }   
+    );
     var response = await http.get(url);
 
-    print(response.body);
+    print( response.body );
   }
 }
